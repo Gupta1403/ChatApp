84321ee24370b9611488a20bd3695e5e6c4ee49e
Gupta1403 <48518912+Gupta1403@users.noreply.github.com>
2020-04-15 22:26:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static( 'client/build' ));
 
     app.get( '*', (req, res) => {
-        res.sendFile( path.join(__dirname, 'client', 'build') )
+        res.sendFile( path.join(__dirname, 'client', 'build','index.html') )
     })
 }
 
